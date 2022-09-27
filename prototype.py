cc7a855fe090869575309d144e6f03af38f087aa
charlestian23 <charles.tian23@gmail.com>
2022-09-27 12:48:58 -0400

    Update prototype.py

--- prototype.py
+++ prototype.py
@@ -16,7 +16,7 @@ pprint(account_data)
 puuid = account_data["puuid"]
 for i in range(3):
     try:
-        match_history = valo.get_match_history_by_puuid("v3", "na", puuid, 10, game_mode="competitive")
+        match_history = valo.get_match_history_by_puuid("v3", "na", puuid, 1, game_mode="competitive")
     except valo.exceptions.valo_api_exception.ValoAPIException as e:
         if i != 2:
             print("Trying again")
@@ -24,4 +24,18 @@ for i in range(3):
         else:
             raise e
 match_history = [match.to_dict() for match in match_history]
-pprint(match_history)
+# pprint(match_history)
+
+for match in match_history:
+    rounds = [rnd.to_dict() for rnd in match["rounds"]]
+    pprint(rounds)
+    round_number = 1
+    for rnd in rounds:
+        player_stats = [stats.to_dict() for stats in rnd["player_stats"]]
+        print("ROUND", round_number)
+        round_kills = []
+        for player_stat in player_stats:
+            round_kills.extend([kill.to_dict() for kill in player_stat["kill_events"]])
+        round_kills = sorted(round_kills, key=lambda x: x["kill_time_in_round"])
+        pprint(round_kills)
+        round_number += 1
